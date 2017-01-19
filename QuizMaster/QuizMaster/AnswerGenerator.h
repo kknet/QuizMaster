84215da0402cd807950cff7bde8c38e1ecea6856
Alique Williams <Alique.Williams@icloud.com>
2017-01-19 14:32:33 -0500

    Still working on query

--- QuizMaster/QuizMaster/AnswerGenerator.h
+++ QuizMaster/QuizMaster/AnswerGenerator.h
@@ -2,4 +2,6 @@
 
 @interface AnswerGenerator: NSObject
 
+-(void)generationRandomQuestions;
+
 @end
--- QuizMaster/QuizMaster/AnswerGenerator.m
+++ QuizMaster/QuizMaster/AnswerGenerator.m
@@ -1,13 +1,24 @@
 #import "AnswerGenerator.h"
+#import "jServices.h"
+
+@protocol AnswerGeneratorDelegate <NSObject>
+
+@optional
+- (void)didGetJSON;
+
+@end
 
 @interface AnswerGenerator ()
+
+@property (weak, nonatomic) jServices *web;
+@property(strong, nonatomic) id <AnswerGeneratorDelegate> delegate;
 
 @end
 
 @implementation AnswerGenerator
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
+-(void)generationRandomQuestions {
+    self.web.GetRandomQuery;
 }
 
 
