7cf27f6b710c3866915b2c66673ddbf321ccad70
Juliana Strawn <julianastrawn@gmail.com>
2017-01-19 16:46:50 -0500

    UI constraints

--- QuizMaster/QuizMaster/ViewController.h
+++ QuizMaster/QuizMaster/ViewController.h
@@ -11,6 +11,7 @@
 @interface ViewController : UIViewController
 
 @property (weak, nonatomic) IBOutlet UILabel *highScoreDisplay;
+@property (weak, nonatomic) IBOutlet UIButton *startGame;
 
 @end
 
